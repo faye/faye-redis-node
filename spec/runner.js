b8313b86e3eaad7886896a5c1ecfb2f555403e65
James Coglan <jcoglan@gmail.com>
2012-01-22 21:43:22 +0000

    Bump Faye submodule.

--- spec/runner.js
+++ spec/runner.js
@@ -1,7 +1,7 @@
 require('jsclass')
 JS.require('JS.Range', 'JS.Test')
 
-require('../vendor/faye/build/faye-node')
+Faye = require('../vendor/faye/build/faye-node')
 require('../vendor/faye/spec/javascript/engine_spec')
 require('./redis_engine_spec')
 
