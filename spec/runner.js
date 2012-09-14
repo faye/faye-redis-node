3d4fcec51bf40ff5bc2ce261836cebc5375d0464
James Coglan <jcoglan@gmail.com>
2012-09-14 07:48:30 +0100

    Fix path to faye library.

--- spec/runner.js
+++ spec/runner.js
@@ -1,7 +1,7 @@
 require('jsclass')
 JS.require('JS.Range', 'JS.Test')
 
-Faye = require('../vendor/faye/build/faye-node')
+Faye = require('../vendor/faye/build/node/faye-node')
 require('../vendor/faye/spec/javascript/engine_spec')
 require('./faye_redis_spec')
 
